46e6909a39bf43cf1744e8b056774f1e5d76c67d
student <student@ICL-TOP-SMLL-26>
2025-04-16 12:27:15 +0500

    Update code

--- Card/Program.cs
+++ Card/Program.cs
@@ -15,6 +15,7 @@ namespace Card
             ScoreCard customer2 = new ScoreCard(32);
             ScoreCard customer3 = new ScoreCard(264);
             ScoreCard customer4 = new ScoreCard(159);
+            ScoreCard customer5 = new ScoreCard(0);
 
             Console.ReadKey();
         }
@@ -29,15 +30,22 @@ namespace Card
         {
             Score = score;
             CalculateDiscount();
-            Console.WriteLine($"Customer discount = {Discount * 100}% \n");
+            if (Discount >= 0)
+                Console.WriteLine($"Customer discount = {Discount * 100}% \n");
         }
 
         public void CalculateDiscount()
         {
-            if (Score > 0 && Score < 100) Discount = 0.01f;
+            if (Score >= 0 && Score < 100) Discount = 0.01f;
             else if (Score >= 100 && Score < 200) Discount = 0.03f;
             else if (Score >= 200 && Score < 500) Discount = 0.05f;
             else if (Score >= 500) Discount = 0.1f;
+            else if (Score < 0)
+            {
+                Console.WriteLine("Score has been less then 0");
+                Discount = -1;
+                return;
+            };
         }
     }
 }
